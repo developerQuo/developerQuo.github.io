ea66b449d2e16149a8db4d757111536a7473ccf6
changmin <developerQuo@gmail.com>
2023-07-23 01:11:21 +0900

    auth

---
+++ src/auth.ts
@@ -0,0 +1,41 @@
+function visibleEl(el: HTMLElement, visible: boolean) {
+  el.style.display = visible ? 'block' : 'none'
+}
+
+function login() {
+  const el = document.querySelector<HTMLDivElement>('.login')!
+  visibleEl(el, true)
+
+  // logout func
+  const logoutbtn = el.querySelector('#logout')!
+  const onLogout = () => {
+    localStorage.removeItem('user')
+    visibleEl(el, false)
+    logout()
+  }
+  logoutbtn.addEventListener('click', onLogout)
+}
+
+function logout() {
+  const el = document.querySelector<HTMLDivElement>('.logout')!
+  visibleEl(el, true)
+
+  // login func
+  const form = el.querySelector('form')!
+  const onSubmit = (event: SubmitEvent) => {
+    event.preventDefault()
+
+    const values = new FormData(form)
+    // TODO: ecrypt password
+    const user = {
+      username: values.get('username'),
+      password: values.get('password'),
+    }
+    localStorage.setItem('user', JSON.stringify(user))
+    visibleEl(el, false)
+    login()
+  }
+  form.addEventListener('submit', onSubmit)
+}
+
+export { login, logout }
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,43 @@
 import './style.css'
+import { login, logout } from './auth.ts'
 import setupClock from './clock.ts'
+import { User } from '../type/user.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <h1>졸업 작품</h1>
     <div id="clock"></div>
+    <div class="login">
+      <div class="card">
+        <button id="logout" type="button">Logout</button>
+      </div>
+      <p class="read-the-docs">
+        Click on the Vite and TypeScript logos to learn more
+      </p>
+    </div>
+    <div class="logout">
+      <form>
+        <div>
+          <label for="username">Username</label>
+          <input name="username" type="text" required />
+        </div>
+        <div>
+          <label for="password">Password</label>
+          <input name="password" type="password" required />
+        </div>
+        <div class="card">
+          <button>Login</button>
+        </div>
+      </form>
+    </div>
   </div>
 `
 
 setupClock(document.querySelector<HTMLDivElement>('#clock')!)
+
+const strUser = localStorage.getItem('user')
+if (strUser) {
+  login()
+} else {
+  logout()
+}
